9ba1cb86f001c8cd9f76c9bf29bd9e3094cea544
Guilherme <guilhermematosn1@gmail.com>
2024-03-03 23:03:01 +0000

    trying to fix something

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,76 +1,71 @@
-import React, { useState } from 'react'
+import React from 'react'
 import computer from "../../assets/project-mockup-example.jpeg"
 import "./Projects.css"
-import project1 from "../../assets/Financial-Tracker.png"
 
-function Projects() {
+function projects() {
     const projectName1 = "Financial Tracker"
     const projectName2 = "Project 2";
     const projectName3 = "Project 3";
-
-    const projectsData = [
-        {
-            name:  "Financial Tracker",
-            description: "This is the first Full-Stack project I've built using the framework React. It helped me understand the basics of it and gave me an idea on how to build some really cool stuff for the future.",
-            image: computer
-        },
-        {
-            name: "Friend's Portfolio",
-            description: "This is a description",
-            image: computer
-        },
-        {
-            name: "Friend's Portfolio",
-            description: "This is a second description",
-            image: computer
-        }
-    ]
-
-    const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
-    const [direction, setDirection] = useState("")
-
-    const goToNextProject = () => {
-        setDirection("next")
-        setCurrentProjectIndex((prevIndex) => (prevIndex + 1) % projectsData.length)
-    }
-
-    const goToPreviousProject = () => {
-        setDirection("prev")
-        setCurrentProjectIndex((prevIndex) => (prevIndex - 1 + projectsData.length) % projectsData.length)
-    }
-
-    const currentProject = projectsData[currentProjectIndex]
   
   return (
     <section id='projects'  className='projects-section'>
         <div className='main-container'>
             <h2 className='heading-2-projects'>
                 <span className='heading-sec-main'>Projects</span>
+                <span className='heading-sec-sub'>These are the projects I'd like you to see</span>
             </h2>
-            <div className="projects-content">
-                <div className="projects-row">
-                <button className='arrow-button' onClick={goToPreviousProject}>{"<"}</button>
+            <div className='projects-content'>
+                <div className='projects-row'>
                     <div className='project-row-image'>
-                        <img src={currentProject.image} className='laptop-img' alt='Project Screenshot' loading='lazy'/>
+                        <img src={computer} className='laptop-img' alt='Project Screenshot' loading='lazy'/>
                     </div>
                     <div className='projects-row-content'>
-                        <h3 className='projects-row-content-title'>{currentProject.name}</h3>
-                        <p className='projects-row-content-description'>{currentProject.description}</p>
+                        <h3 className='projects-row-content-title'>{projectName1}</h3>
+                        <p className='projects-row-content-description'>
+                        This is the first <strong>Full-Stack</strong> project I've built using the framework <strong>React</strong>.
+                        It helped me understand the basics of it and gave me an idea on how to build some really 
+                        cool stuff for the future.
+                        </p>
                         <a className='button'>Case Study</a>
                     </div>
-                    <button className='arrow-button' onClick={goToNextProject}>{">"}</button>
+                </div>
+
+                <div className='projects-row'>
+                    <div className='project-row-image'>
+                        <img src={computer} className='laptop-img' alt='Project Screenshot' loading='lazy'/>
+                    </div>
+                    <div className='project-row-content'>
+                        <h3 className='projects-row-content-title'>{projectName2}</h3>
+                        <p className='projects-row-content-description'>
+                        This is the first <strong>Full-Stack</strong> project I've built using the framework <strong>React</strong>.
+                        It helped me understand the basics of it and gave me an idea on how to build some really 
+                        cool stuff for the future.
+                        </p>
+                        <a className='button'>Case Study</a>
+                    </div>
+                </div>
+
+                <div className='projects-row'>
+                    <div className='project-row-image'>
+                        <img src={computer} className='laptop-img' alt='Project Screenshot' loading='lazy'/>
+                    </div>
+                    <div className='project-row-content'>
+                        <h3 className='projects-row-content-title'>{projectName3}</h3>
+                        <p className='projects-row-content-description'>
+                        This is the first <strong>Full-Stack</strong> project I've built using the framework <strong>React</strong>.
+                        It helped me understand the basics of it and gave me an idea on how to build some really 
+                        cool stuff for the future.
+                        </p>
+                        <a className='button'>Case Study</a>
+                    </div>
                 </div>
             </div>
-            <div className='projects-indicators'>
-                {projectsData.map((project, index) => (
-                    <div key={index}
-                    className={`projects-indicator ${index === currentProjectIndex ? 'active' : ""}`}
-                    onClick={() => setCurrentProjectIndex(index)}/>
-                ))}
-            </div>
+
+
+
         </div>
     </section>
   )
 }
 
-export default Projects
+export default projects
